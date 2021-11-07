5cd9db5bf2efd46ab9eb0ed6a9ecffb205ae83d2
Joao Filipe <florentinojoaofilipe644@gmail.com>
2021-11-07 13:05:33 -0300

    Envio da questão 04

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title> Questão 4 </title>
+</head>
+<body>
+   
+   <form action="insert.php" method="post">
+   	
+    <p> Nome: <input type="text" name="nome"> </p>
+    <p> E-mail: <input type="email" name="e-mail"> </p>
+    <p> Idade: <input type="number" name="idade"> </p>
+
+     <button type="submit"> Inserir dados </button>
+   </form>
+
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,8 @@
+<?php
+$Nome = $_POST['nome'];
+$Email = $_POST['e-mail'];
+$Idade = $_POST['idade'];
+
+echo "O nome selecionado é: ".$Nome."<br>";
+echo "Seu E-mail é: ".$Email."<br>";
+echo "Sua idade é: ".$Idade."<br>";
